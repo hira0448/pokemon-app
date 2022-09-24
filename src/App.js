57cb390074f4226bf3f3995322ba3f17a518fa07
hirano <rhi691508@gmail.com>
2022-09-25 00:00:59 +0900

    Set a function to get all Pokemon data#3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
   const initialURl = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon();
+    };
+  },[]);
 
   return (
     <div className="App">
