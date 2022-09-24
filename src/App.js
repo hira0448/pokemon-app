ce415a9a44b941f88917fa4251e5ef87d40ab1ad
hirano <rhi691508@gmail.com>
2022-09-25 00:11:06 +0900

    Processing to wait until data can be obtained using the fetch function#3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
   const initialURl = 'https://pokeapi.co/api/v2/pokemon'
@@ -7,8 +8,10 @@ function App() {
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
-      let res = await getAllPokemon();
+      let res = await getAllPokemon(initialURl);
+      console.log(res);
     };
+    fetchPokemonData()
   },[]);
 
   return (
