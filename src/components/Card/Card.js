8dbd6d6a7b21b64df07684723774b4990bb2c532
hirano <rhi691508@gmail.com>
2022-10-06 01:18:25 +0900

    Disabled Pretter#9

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Card = ({pokemon}) => {
+const Card = ({ pokemon }) => {
   return (
-    <div className='card'>
-  <div className="cardImg">
-    <img src={pokemon.sprites.front_default} alt=""/>
-  </div>
-  <h3 className='cardName'>{pokemon.name}</h3>
-  <div className="cardTypes">
-    <div>タイプ</div>
-    {pokemon.types.map((type) => {
-      return <div>
-        <span className='typeName'>{type.type.name}</span>
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} alt="" />
       </div>
-    })}
-  </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((type) => {
+          return (
+            <div>
+              <span className="typeName">{type.type.name}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
