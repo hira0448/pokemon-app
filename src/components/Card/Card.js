5f741b2cc8f78535c0a83792c69086e2a927f8f3
hirano <rhi691508@gmail.com>
2022-10-07 14:05:38 +0900

    Get Weight, Height and Abilities from Pokemon API#9

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -17,6 +17,19 @@ const Card = ({ pokemon }) => {
           );
         })}
       </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">重さ：{pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">高さ：{pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">
+            アビリティ：{pokemon.abilities[0].ability.name}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
